09d4885e0c5d1bf21c07fc557a0704caee441c05
Arion Lauriano <camila@lauriano.org>
2026-07-10 11:37:55 -0300

    Episode 3
    Slash commands

--- Ezra_Learning/main.py
+++ Ezra_Learning/main.py
@@ -1,12 +1,21 @@
 import discord
+from discord.ext import commands
+from discord import app_commands
+
 import confidentials
 
-# Busca o token do bot em confidentials.py
-Token = confidentials.BotToken
-
-class Client(discord.Client):
+class Client(commands.Bot):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+
+        try:
+            ServerID = confidentials.DevelopmentServerID
+            guild = discord.Object(id=ServerID)
+            synced = await self.tree.sync(guild=guild)
+            print(f'Synced {len(synced)} comands to guild {guild.id}')
+        except Exception as e:
+            print(f'Error syncing commands: {e}')
+
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -22,6 +31,19 @@ class Client(discord.Client):
 
 intents = discord.Intents.default()
 intents.message_content = True
+client = Client(command_prefix="!", intents=intents)
 
-client = Client(intents=intents)
+ServerID = confidentials.DevelopmentServerID
+GUILD_ID = discord.Object(id=ServerID)
+
+@client.tree.command(name="helloo", description="Say Hello.", guild=GUILD_ID)
+async def sayHello(interaction: discord.Interaction):
+    await interaction.response.send_message("Hi there!")
+
+@client.tree.command(name="printer", description="I will print whatever you give me!", guild=GUILD_ID)
+async def sayHello(interaction: discord.Interaction, printer: str):
+    await interaction.response.send_message(printer)
+
+# Busca o token do bot em confidentials.py
+Token = confidentials.BotToken
 client.run(Token)
